f90dfda89612836b6225567f5f89e8bfe32398d2
Atharv Borse <61404222+atharvborse30@users.noreply.github.com>
2025-03-10 23:13:29 +0530

    Add files via upload

---
+++ Day_3/4_Classes/8_Structs/Vector3f.hpp
@@ -0,0 +1,18 @@
+#ifndef VECTOR3F_HPP
+#define VECTOR3F_HPP
+
+// struct and class both are used for defined user defined type ,
+// the difference is that class is by default private and struct is by default public
+
+// A Vector3f data type, storing floats
+struct Vector3f{
+
+    
+
+    //Constructor
+    Vector3f();
+
+    float x,y,z;
+};
+
+#endif
